396893741f2c54ae37a04384e4c8bea483168cda
ChieftainY2k <ChieftainY2k@gmail.com>
2024-09-29 23:38:57 +0200

    experiments

--- process.sh
+++ process.sh
@@ -3,10 +3,14 @@
 set -e
 
 # Define the number of GPUs and how many times to run the command per GPU
-GPUS=(0 1)
-RUNS_PER_GPU=3
 
-for gpu in "${GPUS[@]}"; do
+# Detect number of GPUS from nvisia-smi
+GPUS=$(nvidia-smi --query-gpu=name --format=csv,noheader | wc -l)
+#GPUS=(0 1)
+RUNS_PER_GPU=4
+
+#for gpu in "${GPUS[@]}"; do
+for ((gpu = 0; gpu < GPUS; gpu++)); do
   for ((i = 1; i <= RUNS_PER_GPU; i++)); do
     screen docker run --rm --gpus all \
       -e CUDA_VISIBLE_DEVICES=$gpu \
