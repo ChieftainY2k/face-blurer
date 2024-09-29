4c73599e54a04288fb3daa2f8b6a008fd12dc701
ChieftainY2k <ChieftainY2k@gmail.com>
2024-09-29 14:55:54 +0200

    experiments

--- app/blur_faces_slow.py
+++ app/blur_faces_slow.py
@@ -78,7 +78,7 @@ def blur_faces_in_directory(input_dir, output_dir):
 
         lock_path = output_path + '.lock'
         try:
-            fd_lock = os.open(lock_path, os.O_CREAT | os.O_WRONLY)
+            fd_lock = os.open(lock_path, os.O_CREAT | os.O_WRONLY | os.O_EXCL)
             fcntl.flock(fd_lock, fcntl.LOCK_EX | fcntl.LOCK_NB)
         except BlockingIOError:
             print(f", skipping as lock file {lock_path} is locked by another process", flush=True)
