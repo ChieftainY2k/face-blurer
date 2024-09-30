245d78c2514138e84c4d06edcff6a68064b4411d
ChieftainY2k <ChieftainY2k@gmail.com>
2024-09-30 14:59:49 +0200

    experiments

---
+++ download_all.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# shellcheck disable=SC2086
+
+set -e
+
+# check variablews
+if [ -z "$THOST" ]; then
+  echo "THOST is not set"
+  exit 1
+fi
+
+if [ -z "$TPORT" ]; then
+  echo "TPORT is not set"
+  exit 1
+fi
+
+if [ -z "$TUSER" ]; then
+  echo "$TUSER is not set"
+  exit 1
+fi
+
+# Download videos
+rsync -avz --partial --info=progress2 -e "ssh -p $TPORT" $TUSER@$THOST:/home/$TUSER/face-blurer/output/vi* /tmp/output-$THOST/
+
+# Download all
+rsync -avz --partial --info=progress2 -e "ssh -p $TPORT" $TUSER@$THOST:/home/$TUSER/face-blurer/output/ /tmp/output-$THOST/
