d76612271f65bb134c7b860a6028665e68eac5ce
ChieftainY2k <ChieftainY2k@gmail.com>
2024-09-30 09:53:51 +0200

    experiments

--- process.sh
+++ process.sh
@@ -13,15 +13,7 @@ echo "Will run $RUNS_PER_GPU times per GPU"
 #for gpu in "${GPUS[@]}"; do
 for ((gpu = 0; gpu < GPUS; gpu++)); do
   for ((i = 1; i <= RUNS_PER_GPU; i++)); do
-    echo "Running on GPU $gpu, run $i"
-    screen docker run --rm --gpus all \
-      -e CUDA_VISIBLE_DEVICES=$gpu \
-      -e DEBUG=1 \
-      -v ./app:/app \
-      -v ./input:/input:ro \
-      -v ./output:/output \
-      -v /tmp/blurer-cache/deepface:/root/.deepface \
-      -v /tmp/blurer-cache/root:/root/.cache \
-      blurer python blur_faces_slow.py
-  done
+    ./process_single.sh $gpu
+    sleep 20
+done
 done
---
+++ process_single.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# shellcheck disable=SC2086
+set -e
+
+# get GPU from arg
+GPU=$1
+
+echo "Running on GPU $GPU ..."
+docker run --rm --gpus all \
+  -e CUDA_VISIBLE_DEVICES=$GPU \
+  -e DEBUG=1 \
+  -v ./app:/app \
+  -v ./input:/input:ro \
+  -v ./output:/output \
+  -v /tmp/blurer-cache/deepface:/root/.deepface \
+  -v /tmp/blurer-cache/root:/root/.cache \
+  blurer python blur_faces_slow.py
+pause 'Press [Enter] key to continue...'
