7820cf8b67242350d320a21d08fe258f619b9223
ChieftainY2k <ChieftainY2k@gmail.com>
2024-09-30 09:56:57 +0200

    experiments

--- process_all.sh
+++ process_all.sh
@@ -13,7 +13,8 @@ echo "Will run $RUNS_PER_GPU times per GPU"
 #for gpu in "${GPUS[@]}"; do
 for ((gpu = 0; gpu < GPUS; gpu++)); do
   for ((i = 1; i <= RUNS_PER_GPU; i++)); do
-    screen ./process_single.sh $gpu
+    echo "Running on GPU $gpu ..."
+    screen -S GPU-$gpu-$i ./process_single.sh $gpu
     #sleep 20
 done
 done
