5b8ff2dab3feec569c1f576af7145de71226abc4
ChieftainY2k <ChieftainY2k@gmail.com>
2024-09-30 20:28:45 +0200

    experiments

---
+++ download_output.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# shellcheck disable=SC2086
+
+set -e
+
+# check variablews
+if [ -z "$THOST" ]; then
+  echo "THOST is not set"
+  exit 1
+fi
+
+if [ -z "$TPORT" ]; then
+  echo "TPORT is not set"
+  exit 1
+fi
+
+if [ -z "$TUSER" ]; then
+  echo "$TUSER is not set"
+  exit 1
+fi
+
+#REMOTE_SOURCE={$1:-"vi*"}
+REMOTE_SOURCE={$1:-"*"}
+
+# Download videos
+rsync -avz --partial --info=progress2 -e "ssh -p $TPORT" $TUSER@$THOST:/home/$TUSER/face-blurer/output/$REMOTE_SOURCE /tmp/output-$THOST/
+
+# Download all
+#rsync -avz --partial --info=progress2 -e "ssh -p $TPORT" $TUSER@$THOST:/home/$TUSER/face-blurer/output/ /tmp/output-$THOST/
--- upload_input.sh
+++ upload_input.sh
@@ -19,5 +19,5 @@ if [ -z "$TUSER" ]; then
   exit 1
 fi
 
-INPUT={$1:-"./input/vi*"}
-rsync -avz --partial --info=progress2 --delete -e "ssh -p $TPORT" $INPUT $TUSER@$THOST:/home/$TUSER/face-blurer/input/
+LOCAL_SOURCE={$1:-"./input/vi*"}
+rsync -avz --partial --info=progress2 --delete -e "ssh -p $TPORT" $LOCAL_SOURCE $TUSER@$THOST:/home/$TUSER/face-blurer/input/
