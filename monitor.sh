7681b8d132054b07ccab97607dad6c36eba5e0aa
ChieftainY2k <ChieftainY2k@gmail.com>
2024-10-02 14:44:45 +0200

    experiments

--- monitor.sh
+++ monitor.sh
@@ -3,4 +3,4 @@
 # change window title
 echo -ne "\033kINFO\033\\"
 
-watch -c -n 3 "uptime; free; pydf; nvidia-smi; ls output/*.lock"
+watch -d -c -n 3 "uptime; free; pydf; nvidia-smi; ls output/*.lock"
