7b05eeda45236fd58184540c860fc19a0a86844f
ChieftainY2k <ChieftainY2k@gmail.com>
2024-10-05 21:30:55 +0200

    experiments

--- compose_output_video.sh
+++ compose_output_video.sh
@@ -70,8 +70,10 @@ if [ "$DEBUG" == "1" ]; then
   FILE_MARKER="debug"
 fi
 
+mkdir -p "output/video"
+
 # Use variables in the ffmpeg command
-COMMAND="ffmpeg -r \"$FPS\" -hwaccel \"cuda\" -f image2 -s \"$RESOLUTION\" -i \"output/frame_%10d.png.${FILE_MARKER}.png\" -c:v h264_nvenc -preset slow -cq 20  \"output/video-${MD5_HASH}-${RESOLUTION}-${FPS}fps-${FILE_MARKER}-th${THRESHOLD}-be${BLUR_EXTRA}-ba${BLUR_AHEAD}-bb${BLUR_BACK}.mp4\" "
+COMMAND="ffmpeg -r \"$FPS\" -hwaccel \"cuda\" -f image2 -s \"$RESOLUTION\" -i \"output/frame_%10d.png.${FILE_MARKER}.png\" -c:v h264_nvenc -preset slow -cq 20  \"output/video/video-${MD5_HASH}-${RESOLUTION}-${FPS}fps-${FILE_MARKER}-th${THRESHOLD}-be${BLUR_EXTRA}-ba${BLUR_AHEAD}-bb${BLUR_BACK}.mp4\" "
 
 # show command , wait for ENTER
 log_message "About to exec the command: $COMMAND"
