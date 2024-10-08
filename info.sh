9b16e9762f6819ef62ffc709fa5f9eb0544399fd
ChieftainY2k <ChieftainY2k@gmail.com>
2024-10-08 12:25:33 +0200

    experiments

--- info.sh
+++ info.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+# shellcheck disable=SC2016
 
 . ./functions.sh
 
 # change window title
 set_sceen_name "INFO"
 
-watch -c -n 3 "uptime; free; pydf; nvidia-smi; ls output/*.lock"
+watch -c -n 3 'uptime; free; pydf; nvidia-smi; ls output/*.lock; for file in output/meta*; do echo "$file : $(tail -n 1 "$file")"; done'
